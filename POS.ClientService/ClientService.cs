4ebec5aadff13f5b3dca022e89b28646e5cc0f83
ashok subedi <ashok.subedi5@gmail.com>
2019-09-01 00:37:27 +0545

    Stages Commit

--- POS.ClientService/ClientService.cs
+++ POS.ClientService/ClientService.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Http.SelfHost;
 using System.Configuration;
 using System.Reflection;
+using System.ServiceProcess;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
 
 
 namespace POSService
