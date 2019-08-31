dab19c813b3d1959f5e8b8b50d02a04ec820a67f
ashok subedi <ashok.subedi5@gmail.com>
2019-09-01 00:38:06 +0545

    Update SP, change report sorting and filtering to serverside

--- POS.ClientService/ClientService.cs
+++ POS.ClientService/ClientService.cs
@@ -1,9 +1,16 @@
 ﻿using System;
-using System.Configuration;
-using System.Reflection;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
 using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.SelfHost;
+using System.Configuration;
+using System.Reflection;
 
 
 namespace POSService
--- POS.ClientService/POSClientController.cs
+++ POS.ClientService/POSClientController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace POSService
 {
-    public class POSClientController : ApiController
+   public class POSClientController: ApiController
     {
         public string GetComputerName()
         {
